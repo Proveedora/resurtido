52fd9486b9d8c36691ccb7d0830f40e6a05574a2
jdbv1982 <jdbardav@gmail.com>
2015-05-04 12:14:48 -0500

    Se arreglan errores

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,12 +3,13 @@ import prov.res.catalogos.Articulo
 import prov.res.catalogos.Proveedor
 import prov.res.importacion.MaeartService
 import prov.res.importacion.MaeproService
+import prov.res.importacion.MaeagtService
 import prov.res.usuarios.Rol
 import prov.res.usuarios.Usuario
 import prov.res.almacenes.Almacen
 import prov.res.empresa.Empresa
 import prov.res.sucursal.Sucursal
-import prov.res.importacion.MaeagtService
+import prov.res.catalogos.Unidad
 
 class BootStrap {
 
@@ -21,12 +22,8 @@ class BootStrap {
         Empresa empresa = new Empresa(CP:"68050",domicilio:"Nicolas del puerto",estado:"Oaxaca",municipio:"Oaxaca de juarez",pais:"Mexico",razonSocial:"Proveedora Escolar S. de R.L.",regimen:"Mediano",RFC:"PES12345678").save()
         Almacen almacen=new Almacen(clave:"01",direccionDB:"C:/base/winvecaja.fdb",nombre:"Merced").save()
         new Sucursal(clave:"01",,nombre:"prueba",serie:"TA1",ubicacion:"Merced",empresa: empresa).addToAlmacenes(almacen).save()
-        //new Usuario(agente:"dsd", contraseña: "david", correo:"admin@admin.com", domicilio:"asdas dasd asd asd",nombre:"David Barranco", telefono:"89080098",usuario:"david").save()
     	new Usuario(agente:"dsd", contraseña: "david", correo:"admin@admin.com", domicilio:"asdas dasd asd asd",nombre:"David Barranco", telefono:"89080098",usuario:"david").addToRoles(rol).save()
         new Unidad(clave:"PZA",nombre:"Pieza");
-        for (int i = 0; i < 50; i++) {
-            new Agente(numagt: fakerService.zipCode(), nombreagt: fakerService.name(), numalm: fakerService.bothify('##')).save()
-        }
 
         if(!Proveedor.count()){
             def maeproService = new MaeproService()
@@ -38,10 +35,10 @@ class BootStrap {
             maeartService.getArticulos()
         }
 
-
-
-
-
+        if(!Agente.count()){
+            def maeagtService = new MaeagtService()
+            maeagtService.getAgentes()
+        }
 
     }
     def destroy = {
--- test/unit/prov/res/catalogos/UnidadControllerSpec.groovy
+++ test/unit/prov/res/catalogos/UnidadControllerSpec.groovy
@@ -1,7 +1,6 @@
 package prov.res.catalogos
 
 
-
 import grails.test.mixin.*
 import spock.lang.*
 
@@ -17,136 +16,136 @@ class UnidadControllerSpec extends Specification {
 
     void "Test the index action returns the correct model"() {
 
-        when:"The index action is executed"
-            controller.index()
+        when: "The index action is executed"
+        controller.index()
 
-        then:"The model is correct"
-            !model.unidadInstanceList
-            model.unidadInstanceCount == 0
+        then: "The model is correct"
+        !model.unidadInstanceList
+        model.unidadInstanceCount == 0
     }
 
     void "Test the create action returns the correct model"() {
-        when:"The create action is executed"
-            controller.create()
+        when: "The create action is executed"
+        controller.create()
 
-        then:"The model is correctly created"
-            model.unidadInstance!= null
+        then: "The model is correctly created"
+        model.unidadInstance != null
     }
 
     void "Test the save action correctly persists an instance"() {
 
-        when:"The save action is executed with an invalid instance"
-            request.contentType = FORM_CONTENT_TYPE
-            request.method = 'POST'
-            def unidad = new Unidad()
-            unidad.validate()
-            controller.save(unidad)
+        when: "The save action is executed with an invalid instance"
+        request.contentType = FORM_CONTENT_TYPE
+        request.method = 'POST'
+        def unidad = new Unidad()
+        unidad.validate()
+        controller.save(unidad)
 
-        then:"The create view is rendered again with the correct model"
-            model.unidadInstance!= null
-            view == 'create'
+        then: "The create view is rendered again with the correct model"
+        model.unidadInstance != null
+        view == 'create'
 
-        when:"The save action is executed with a valid instance"
-            response.reset()
-            populateValidParams(params)
-            unidad = new Unidad(params)
+        when: "The save action is executed with a valid instance"
+        response.reset()
+        populateValidParams(params)
+        unidad = new Unidad(params)
 
-            controller.save(unidad)
+        controller.save(unidad)
 
-        then:"A redirect is issued to the show action"
-            response.redirectedUrl == '/unidad/show/1'
-            controller.flash.message != null
-            Unidad.count() == 1
+        then: "A redirect is issued to the show action"
+        response.redirectedUrl == '/unidad/show/1'
+        controller.flash.message != null
+        Unidad.count() == 1
     }
 
     void "Test that the show action returns the correct model"() {
-        when:"The show action is executed with a null domain"
-            controller.show(null)
+        when: "The show action is executed with a null domain"
+        controller.show(null)
 
-        then:"A 404 error is returned"
-            response.status == 404
+        then: "A 404 error is returned"
+        response.status == 404
 
-        when:"A domain instance is passed to the show action"
-            populateValidParams(params)
-            def unidad = new Unidad(params)
-            controller.show(unidad)
+        when: "A domain instance is passed to the show action"
+        populateValidParams(params)
+        def unidad = new Unidad(params)
+        controller.show(unidad)
 
-        then:"A model is populated containing the domain instance"
-            model.unidadInstance == unidad
+        then: "A model is populated containing the domain instance"
+        model.unidadInstance == unidad
     }
 
     void "Test that the edit action returns the correct model"() {
-        when:"The edit action is executed with a null domain"
-            controller.edit(null)
+        when: "The edit action is executed with a null domain"
+        controller.edit(null)
 
-        then:"A 404 error is returned"
-            response.status == 404
+        then: "A 404 error is returned"
+        response.status == 404
 
-        when:"A domain instance is passed to the edit action"
-            populateValidParams(params)
-            def unidad = new Unidad(params)
-            controller.edit(unidad)
+        when: "A domain instance is passed to the edit action"
+        populateValidParams(params)
+        def unidad = new Unidad(params)
+        controller.edit(unidad)
 
-        then:"A model is populated containing the domain instance"
-            model.unidadInstance == unidad
+        then: "A model is populated containing the domain instance"
+        model.unidadInstance == unidad
     }
 
     void "Test the update action performs an update on a valid domain instance"() {
-        when:"Update is called for a domain instance that doesn't exist"
-            request.contentType = FORM_CONTENT_TYPE
-            request.method = 'PUT'
-            controller.update(null)
+        when: "Update is called for a domain instance that doesn't exist"
+        request.contentType = FORM_CONTENT_TYPE
+        request.method = 'PUT'
+        controller.update(null)
 
-        then:"A 404 error is returned"
-            response.redirectedUrl == '/unidad/index'
-            flash.message != null
+        then: "A 404 error is returned"
+        response.redirectedUrl == '/unidad/index'
+        flash.message != null
 
 
-        when:"An invalid domain instance is passed to the update action"
-            response.reset()
-            def unidad = new Unidad()
-            unidad.validate()
-            controller.update(unidad)
+        when: "An invalid domain instance is passed to the update action"
+        response.reset()
+        def unidad = new Unidad()
+        unidad.validate()
+        controller.update(unidad)
 
-        then:"The edit view is rendered again with the invalid instance"
-            view == 'edit'
-            model.unidadInstance == unidad
+        then: "The edit view is rendered again with the invalid instance"
+        view == 'edit'
+        model.unidadInstance == unidad
 
-        when:"A valid domain instance is passed to the update action"
-            response.reset()
-            populateValidParams(params)
-            unidad = new Unidad(params).save(flush: true)
-            controller.update(unidad)
+        when: "A valid domain instance is passed to the update action"
+        response.reset()
+        populateValidParams(params)
+        unidad = new Unidad(params).save(flush: true)
+        controller.update(unidad)
 
-        then:"A redirect is issues to the show action"
-            response.redirectedUrl == "/unidad/show/$unidad.id"
-            flash.message != null
+        then: "A redirect is issues to the show action"
+        response.redirectedUrl == "/unidad/show/$unidad.id"
+        flash.message != null
     }
 
     void "Test that the delete action deletes an instance if it exists"() {
-        when:"The delete action is called for a null instance"
-            request.contentType = FORM_CONTENT_TYPE
-            request.method = 'DELETE'
-            controller.delete(null)
+        when: "The delete action is called for a null instance"
+        request.contentType = FORM_CONTENT_TYPE
+        request.method = 'DELETE'
+        controller.delete(null)
 
-        then:"A 404 is returned"
-            response.redirectedUrl == '/unidad/index'
-            flash.message != null
+        then: "A 404 is returned"
+        response.redirectedUrl == '/unidad/index'
+        flash.message != null
 
-        when:"A domain instance is created"
-            response.reset()
-            populateValidParams(params)
-            def unidad = new Unidad(params).save(flush: true)
+        when: "A domain instance is created"
+        response.reset()
+        populateValidParams(params)
+        def unidad = new Unidad(params).save(flush: true)
 
-        then:"It exists"
-            Unidad.count() == 1
+        then: "It exists"
+        Unidad.count() == 1
 
-        when:"The domain instance is passed to the delete action"
-            controller.delete(unidad)
+        when: "The domain instance is passed to the delete action"
+        controller.delete(unidad)
 
-        then:"The instance is deleted"
-            Unidad.count() == 0
-            response.redirectedUrl == '/unidad/index'
-            flash.message != null
+        then: "The instance is deleted"
+        Unidad.count() == 0
+        response.redirectedUrl == '/unidad/index'
+        flash.message != null
     }
 }
